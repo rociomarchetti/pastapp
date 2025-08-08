5bd004aad03221c29646632c00ceb381a4fd6699
Rocío Marchetti <marchettirociob@gmail.com>
2025-08-08 17:56:58 +0200

    feat(services): add service draft

---
+++ src/app/core/services/recipe.service.ts
@@ -0,0 +1,50 @@
+import { Injectable } from '@angular/core';
+import { Recipe } from '@shared/recipe.model';
+import { BehaviorSubject, map, Observable, of } from 'rxjs';
+import { RECIPES_MOCK } from './__mocks__/recipes.mock';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class RecipeService {
+  private recipesSubject = new BehaviorSubject<Array<Recipe>>(RECIPES_MOCK);
+  recipes$ = this.recipesSubject.asObservable();
+
+  getRecipes$(): Observable<Array<Recipe>> {
+    return this.recipes$;
+  }
+
+  getRecipeById$(id: string): Observable<Recipe | undefined> {
+    return this.recipes$.pipe(
+      map((recipes) => recipes.find((recipe) => recipe.id === id))
+    );
+  }
+
+  addRecipe$(newRecipe: Recipe): Observable<Array<Recipe>> {
+    const updatedList = [...this.currentList, newRecipe];
+    this.updateRecipes(updatedList);
+    return of(updatedList);
+  }
+
+  deleteRecipeById$(id: string): Observable<Array<Recipe>> {
+    const updatedList = this.currentList.filter((recipe) => recipe.id !== id);
+    this.updateRecipes(updatedList);
+    return of(updatedList);
+  }
+
+  updateRecipe$(updatedRecipe: Recipe): Observable<Array<Recipe>> {
+    const updatedList = this.currentList.map((currentRecipe) =>
+      currentRecipe.name === updatedRecipe.name ? updatedRecipe : currentRecipe
+    );
+    this.updateRecipes(updatedList);
+    return of(updatedList);
+  }
+
+  private updateRecipes(recipes: Recipe[]): void {
+    this.recipesSubject.next(recipes);
+  }
+
+  private get currentList(): Recipe[] {
+    return this.recipesSubject.getValue();
+  }
+}
