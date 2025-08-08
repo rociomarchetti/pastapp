83db57b63d94a1c1ac1f4cb0f20f24ef61da82c8
Rocío Marchetti <marchettirociob@gmail.com>
2025-08-08 17:42:30 +0200

    feat(app): generate layout component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Layout } from './core/layout/layout';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Layout],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
 export class App {
   protected readonly title = signal('pastapp');
