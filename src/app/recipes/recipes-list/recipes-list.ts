5f3077a5899f83e3211a018422528a824f11f2dc
Rocío Marchetti <marchettirociob@gmail.com>
2025-08-12 13:16:36 +0200

    feat(recipes): add recipes-list first draft

--- src/app/recipes/recipes-list/recipes-list.ts
+++ src/app/recipes/recipes-list/recipes-list.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject, OnInit } from '@angular/core';
+import { RecipeService } from '@core/services/recipe.service';
+import { Card } from '@shared/ui/card/card';
+import {
+  CardFooterDirective,
+  CardHeaderDirective,
+} from '@shared/ui/card/card.directive';
 
 @Component({
   selector: 'app-recipes-list',
-  imports: [],
+  imports: [CommonModule, Card, CardHeaderDirective, CardFooterDirective],
+  standalone: true,
   templateUrl: './recipes-list.html',
-  styleUrl: './recipes-list.scss'
+  styleUrls: ['./recipes-list.scss'],
 })
-export class RecipesList {
+export class RecipesList implements OnInit {
+  private recipeService = inject(RecipeService);
+  readonly recipes$ = this.recipeService.recipes$;
 
+  ngOnInit(): void {
+    this.recipeService.getRecipes$();
+  }
+
+  onSelectAction(id: string) {
+    console.log('id:', id);
+  }
 }
