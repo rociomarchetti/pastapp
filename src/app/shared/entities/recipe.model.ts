f69394f2ad8f7641b7d1f782c11c1367c4ac61f6
Rocío Marchetti <marchettirociob@gmail.com>
2025-08-14 14:38:11 +0200

    feat(entities): add new properties to Recipe model

--- src/app/shared/entities/recipe.model.ts
+++ src/app/shared/entities/recipe.model.ts
@@ -4,11 +4,19 @@ export interface Ingredient {
   unit: string;
 }
 
+export enum DifficultyLevel {
+  LOW = 'LOW',
+  MEDIUM = 'MEDIUM',
+  HIGH = 'HIGH',
+}
+
 export interface Recipe {
   id: string;
   name: string;
   servings: number;
   prepTimeMinutes: number;
+  difficultyLevel: DifficultyLevel;
+  imgPath: string;
   ingredients: Array<Ingredient>;
   instructions: Array<string>;
 }
