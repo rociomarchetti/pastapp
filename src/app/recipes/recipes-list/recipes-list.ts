26b76192bfc0df04b787191f20c4572ff72a6771
Rocío Marchetti <marchettirociob@gmail.com>
2025-08-14 17:51:55 +0200

    feat(recipes): add recipes-list first structure

--- src/app/recipes/recipes-list/recipes-list.ts
+++ src/app/recipes/recipes-list/recipes-list.ts
@@ -1,15 +1,23 @@
 import { CommonModule } from '@angular/common';
 import { Component, inject, OnInit } from '@angular/core';
 import { RecipeService } from '@core/services/recipe.service';
+import { Recipe } from '@shared/entities/recipe.model';
 import { Card } from '@shared/ui/card/card';
 import {
   CardFooterDirective,
   CardHeaderDirective,
 } from '@shared/ui/card/card.directive';
+import { Modal } from '@shared/ui/modal/modal';
 
 @Component({
   selector: 'app-recipes-list',
-  imports: [CommonModule, Card, CardHeaderDirective, CardFooterDirective],
+  imports: [
+    CommonModule,
+    Card,
+    CardHeaderDirective,
+    CardFooterDirective,
+    Modal,
+  ],
   standalone: true,
   templateUrl: './recipes-list.html',
   styleUrls: ['./recipes-list.scss'],
@@ -18,11 +26,24 @@ export class RecipesList implements OnInit {
   private recipeService = inject(RecipeService);
   readonly recipes$ = this.recipeService.recipes$;
 
+  isModalOpen = false;
+  selectedRecipe?: Recipe;
+
   ngOnInit(): void {
     this.recipeService.getRecipes$();
   }
 
-  onSelectAction(id: string) {
-    console.log('id:', id);
+  onSelectRecipe(recipe: Recipe): void {
+    this.isModalOpen = true;
+    this.selectedRecipe = recipe;
+  }
+
+  onCloseModal(): void {
+    console.log('close modal');
+    this.isModalOpen = false;
+  }
+
+  goToRecipe(): void {
+    console.log(this.selectedRecipe);
   }
 }
