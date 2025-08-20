b7673ebbb75031087253253da91d4b6d6753a2e8
Rocío Marchetti <marchettirociob@gmail.com>
2025-08-20 16:17:36 +0200

    feat(recipes-list): generate recipes-filters component

---
+++ src/app/recipes/recipes-list/ui-recipes-filters/ui-recipes-filters.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ui-recipes-filters',
+  imports: [],
+  templateUrl: './ui-recipes-filters.html',
+  styleUrl: './ui-recipes-filters.scss'
+})
+export class UiRecipesFilters {
+
+}
