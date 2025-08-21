d033ff394d5c76c4aaf29a71c9257ba2f7d51c1d
Rocío Marchetti <marchettirociob@gmail.com>
2025-08-21 15:54:45 +0200

    feat(recipes): generate recipe-modal component

---
+++ src/app/recipes/recipes-list/ui-recipe-modal/ui-recipe-modal.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ui-recipe-modal',
+  imports: [],
+  templateUrl: './ui-recipe-modal.html',
+  styleUrl: './ui-recipe-modal.scss'
+})
+export class UiRecipeModal {
+
+}
