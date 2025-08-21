d7127d2b405eb5245779a673a240870dffc41af4
Rocío Marchetti <marchettirociob@gmail.com>
2025-08-21 14:51:13 +0200

    feat(recipes): generate recipes-list component

---
+++ src/app/recipes/recipes-list/ui-recipes-list/ui-recipes-list.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ui-recipes-list',
+  imports: [],
+  templateUrl: './ui-recipes-list.html',
+  styleUrl: './ui-recipes-list.scss'
+})
+export class UiRecipesList {
+
+}
