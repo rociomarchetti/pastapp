dd0f937e63ae1dfadc8b934547f210fcc310d021
Rocío Marchetti <marchettirociob@gmail.com>
2025-08-22 16:34:27 +0200

    feat(recipes): generate feature-details component

---
+++ src/app/recipes/recipe-details/feature-details/feature-details.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-feature-details',
+  imports: [],
+  templateUrl: './feature-details.html',
+  styleUrl: './feature-details.scss'
+})
+export class FeatureDetails {
+
+}
