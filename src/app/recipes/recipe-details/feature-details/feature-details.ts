0bbba9934066a620d5b69c57e11061383a65619f
Rocío Marchetti <marchettirociob@gmail.com>
2025-08-25 13:36:25 +0200

    feat(recipe-details): add details first structure

--- src/app/recipes/recipe-details/feature-details/feature-details.ts
+++ src/app/recipes/recipe-details/feature-details/feature-details.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { ActivatedRoute } from '@angular/router';
+import { RecipeService } from '@core/services/recipe.service';
 
 @Component({
   selector: 'app-feature-details',
   imports: [],
   templateUrl: './feature-details.html',
-  styleUrl: './feature-details.scss'
+  styleUrl: './feature-details.scss',
 })
 export class FeatureDetails {
+  private route = inject(ActivatedRoute);
+  private recipeService = inject(RecipeService);
 
+  recipeId = this.route.snapshot.paramMap.get('id')!;
+  recipe = toSignal(this.recipeService.getRecipeById$(this.recipeId));
+
+  recipeBg = computed(() =>
+    this.recipe()?.imgPath ? `url("${this.recipe()?.imgPath}")` : 'none'
+  );
 }
