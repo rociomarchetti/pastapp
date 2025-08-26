bf494e213c8272dad2214c762ab5f420a83fe70e
Rocío Marchetti <marchettirociob@gmail.com>
2025-08-26 19:08:21 +0200

    feat(recipe-details): generate ui-details component

---
+++ src/app/recipes/recipe-details/ui-details/ui-details.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ui-details',
+  imports: [],
+  templateUrl: './ui-details.html',
+  styleUrl: './ui-details.scss'
+})
+export class UiDetails {
+
+}
