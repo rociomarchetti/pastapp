ac0e18d79d644618de7b0b1c0424d92040674c1f
Rocío Marchetti <marchettirociob@gmail.com>
2025-08-27 08:31:43 +0200

    feat(recipe-create): generate feature-create component

---
+++ src/app/recipes/recipe-create/feature-create/feature-create.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-feature-create',
+  imports: [],
+  templateUrl: './feature-create.html',
+  styleUrl: './feature-create.scss'
+})
+export class FeatureCreate {
+
+}
