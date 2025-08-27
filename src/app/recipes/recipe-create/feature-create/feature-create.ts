c673142ee8606efd0bc72597c167c08145214028
Rocío Marchetti <marchettirociob@gmail.com>
2025-08-27 12:50:04 +0200

    feat(recipe-create): use recipe-form in create feature

--- src/app/recipes/recipe-create/feature-create/feature-create.ts
+++ src/app/recipes/recipe-create/feature-create/feature-create.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { RecipeService } from '@core/services/recipe.service';
+import { Recipe } from '@shared/entities/recipe.model';
+import { RecipeForm } from '@shared/ui/recipe-form/recipe-form';
 
 @Component({
   selector: 'app-feature-create',
-  imports: [],
+  imports: [RecipeForm],
   templateUrl: './feature-create.html',
-  styleUrl: './feature-create.scss'
+  styleUrl: './feature-create.scss',
 })
 export class FeatureCreate {
+  private recipeService = inject(RecipeService);
 
+  onCreateRecipe(newRecipe: Recipe): void {
+    this.recipeService
+      .addRecipe$(newRecipe)
+      .subscribe((value) => console.log(value));
+  }
 }
