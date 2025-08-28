e5a8421baec2095d3174c9f1f7721c73d44fca79
Rocío Marchetti <marchettirociob@gmail.com>
2025-08-28 11:38:42 +0200

    feat(favourites): generate feature-favourites component

---
+++ src/app/recipes/recipes-favourites/feature-favourites/feature-favourites.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-feature-favourites',
+  imports: [],
+  templateUrl: './feature-favourites.html',
+  styleUrl: './feature-favourites.scss'
+})
+export class FeatureFavourites {
+
+}
