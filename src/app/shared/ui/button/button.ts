54c348465abb45a8cc1b917eda39db529032f9a0
Rocío Marchetti <marchettirociob@gmail.com>
2025-09-01 09:52:52 +0200

    feat(shared): create shared button component

---
+++ src/app/shared/ui/button/button.ts
@@ -0,0 +1,17 @@
+import { Component, input, output } from '@angular/core';
+
+@Component({
+  selector: 'app-button',
+  imports: [],
+  templateUrl: './button.html',
+  styleUrl: './button.scss',
+})
+export class Button {
+  label = input<string>();
+  isDisabled = input(false);
+  clickOutput = output<void>();
+
+  onClicked(): void {
+    this.clickOutput.emit();
+  }
+}
