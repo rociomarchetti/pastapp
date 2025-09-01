86a87aa64a2b7646c21e6383579a96bc8b5f3a30
Rocío Marchetti <marchettirociob@gmail.com>
2025-09-01 11:37:00 +0200

    feat(recipes): create separate component for matches

---
+++ src/app/recipes/recipes-list/ui-recipes-matches/ui-recipes-matches.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ui-recipes-matches',
+  imports: [],
+  templateUrl: './ui-recipes-matches.html',
+  styleUrl: './ui-recipes-matches.scss'
+})
+export class UiRecipesMatches {
+
+}
