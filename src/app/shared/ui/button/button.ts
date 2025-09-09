92725a132a5fb72cfd1a762d2cc0802a07f14972
Rocío Marchetti <marchettirociob@gmail.com>
2025-09-09 11:44:59 +0200

    feat(app): refactor button component with types

--- src/app/shared/ui/button/button.ts
+++ src/app/shared/ui/button/button.ts
@@ -7,11 +7,22 @@ import { Component, input, output } from '@angular/core';
   styleUrl: './button.scss',
 })
 export class Button {
+  type = input<'fill' | 'outline' | 'ghost'>('fill');
   label = input<string>();
   isDisabled = input(false);
   clickOutput = output<void>();
 
   onClicked(): void {
     this.clickOutput.emit();
+  }
+
+  get classes() {
+    const base = 'btn';
+    const variants = {
+      fill: 'btn-fill',
+      outline: 'btn-outline',
+      ghost: 'btn-ghost',
+    };
+    return `${base} ${variants[this.type()]}`;
   }
 }
